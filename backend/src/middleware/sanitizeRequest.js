46905ba385f4ecc35633f587955ff1fafadd3453
Umut Eren Kaplan <kaeruishere@gmail.com>
2026-06-07 14:49:44 +0300

    s

--- backend/src/middleware/sanitizeRequest.js
+++
@@ -1,9 +0,0 @@
-const { sanitize } = require('express-mongo-sanitize');
-
-const sanitizeRequest = (req, res, next) => {
-  if (req.body) req.body = sanitize(req.body);
-  if (req.params) req.params = sanitize(req.params);
-  next();
-};
-
-module.exports = sanitizeRequest;
